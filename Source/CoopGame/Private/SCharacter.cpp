0d6255cbaa9dc35ab9597b693c7ec14d0205a5a4
Gabriel Cabrera <gabycabper237@gmail.com>
2024-09-17 01:52:51 +0200

    Add camera

--- Source/CoopGame/Private/SCharacter.cpp
+++ Source/CoopGame/Private/SCharacter.cpp
@@ -2,12 +2,17 @@
 
 
 #include "SCharacter.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->bUsePawnControlRotation = true;
 
 }
 
